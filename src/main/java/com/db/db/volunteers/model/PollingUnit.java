421948eaaf44784867bbdfbcb6aaa56feaa7a033
Abioye Bankole <bioyexx@yahoo.com>
2019-01-08 07:44:18 +0100

    Now displaying polling unit name in place of polling unit code

--- src/main/java/com/db/db/volunteers/model/PollingUnit.java
+++ src/main/java/com/db/db/volunteers/model/PollingUnit.java
@@ -1,13 +1,22 @@
 package com.db.db.volunteers.model;
 
+import java.io.Serializable;
+import java.util.regex.Pattern;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.PostLoad;
+import javax.persistence.Transient;
 
 @Entity
-public class PollingUnit{
-    
+public class PollingUnit implements Serializable{
+
+    public String getFullCode(){
+		return fullCode;
+	}
+
 	public String getName() {
 		return name;
 	}
@@ -17,11 +26,29 @@ public class PollingUnit{
 	public int getId() {
 		return id;
 	}
+	/*@PostLoad
+	private void onLoad(){	
+		StringBuilder statePart=new StringBuilder("27");
+		String localGovPart=fullCode.substring(2,5);
+		String wardPart=fullCode.substring(5,8);
+		String pollingUnitPart=fullCode.substring(8,12);
+		fullCodeHyphen = statePart.append("-").append(localGovPart).append("-")
+							.append(wardPart).append("-").append(pollingUnitPart).toString();
+		fullCodeSlash=fullCodeHyphen.replace("/","-");
+	}*/
+
 	@Id
 	private int id;
 	
+	//@Transient
+	//private String fullCodeHyphen;	
+	//@Transient
+	//private String fullCodeSlash;
+	@Column(name="pu_loc_id")
+	private String fullCode;
 	@ManyToOne
 	private Ward ward;
+	@Column(name="description")
 	private String name;
 	@Column(name = "pu_id")
 	private int code;
--- src/main/java/com/db/db/volunteers/model/Volunteer.java
+++ src/main/java/com/db/db/volunteers/model/Volunteer.java
@@ -2,10 +2,21 @@ package com.db.db.volunteers.model;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+
+import org.hibernate.annotations.JoinFormula;
 
 @Entity
 public class Volunteer{
+
+    public PollingUnit getPollingUnit() {
+        return pollingUnit;
+    }
+    /*
+    public Ward getWard() {
+        return ward;
+    }*/
 
     public Group getGroup() {
         return group;
@@ -24,6 +35,14 @@ public class Volunteer{
     }
     @ManyToOne
     private Group group;
+    @ManyToOne
+    @JoinFormula(referencedColumnName="pu_loc_id",
+                 value="CONCAT('OG',REPLACE(SUBSTRING(code,3),'-',''))")
+    //convert from 27-01-02-003 to AB0102003
+	//@JoinColumn(name = "full_code_hyphen")
+    private PollingUnit pollingUnit;
+    //@ManyToOne
+    //private Ward ward;
     private String name;
     private String phoneNo;
     private String position;
